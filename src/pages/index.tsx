a69c1a183d91f2ddcd567a42d32b8c49a2715318
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-01-30 20:12:53 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Page({ topics }: Props): React.ReactElement {
         <div className="relative col-span-12 sm:col-start-2 sm:col-span-10 md:col-start-3 md:col-span-8 lg:col-start-4 lg:col-span-6 xl:col-start-5 xl:col-span-4 min-h-screen">
           <div className="absolute top-1/3 inset-x-0 px-2 sm:px-0">
             <h1 className="text-4xl text-center font-bold mb-4">
-              Syntax Recall
+              Search Recall
             </h1>
             <Searchbar items={topics} />
             <p className="text-sm text-center mt-4">
